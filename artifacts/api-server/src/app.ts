47b1b540c592d2e4fb96212966342b3e1e2eb8c3
BHAVYA-SHRIVASTAVA17 <bhavyashrivastav610@gmail.com>
2026-06-19 11:40:20 +0530

    remove duplicate export

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,13 +1,13 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
-import path from "path";
+import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const app: Express = express();
 app.use(
@@ -34,11 +34,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
-const frontendDist = path.join(__dirname, "../../../../artifacts/workshop/dist/public");
+const frontendDist = join(__dirname, "../../../../artifacts/workshop/dist/public");
 app.use(express.static(frontendDist));
 app.get("*splat", (_req, res) => {
-  res.sendFile(path.join(frontendDist, "index.html"));
+  res.sendFile(join(frontendDist, "index.html"));
 });
 
-export default app;
 export default app;
